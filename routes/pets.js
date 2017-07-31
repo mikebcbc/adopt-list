c46f142f4b412af3e636d7b6fcb14559983e4bd6
mikebcbc <mikebconstantino@gmail.com>
2017-07-31 17:16:58 -0400

    GET request /pets

--- routes/pets.js
+++ routes/pets.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const rp = require('request-promise-native');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(req, res) {
+	const petOptions = {
+		uri: 'http://api.petfinder.com/pet.find',
+		qs: {
+			format: 'json',
+			key: process.env.DEV_KEY,
+			location: 34787
+		},
+		json: true
+	}
+	rp(petOptions)
+		.then(function(pets) {
+			console.log(pets.petfinder.pets);
+			res.send(pets.petfinder.pets.pet);
+		})
 });
 
 module.exports = router;
