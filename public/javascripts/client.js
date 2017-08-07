5977617d021cf9cde07ee7ad5f6f7d5acb139198
mikebcbc <mikebconstantino@gmail.com>
2017-08-07 14:02:58 -0400

    Client side implementation of add to list

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,31 @@
+function getPetData(pet) {
+	const petInfo = {
+		name: $(pet).find(".name").text(),
+		description: $(pet).find(".description").text(),
+		contactInfo: {
+			// phone: $(pet).find(".phone").text(),
+			// email: $(pet).find(".email").text()
+			phone: '1231231231', //temporary
+			email: '123@hotmail.com' //temporary
+		}
+	}
+	return petInfo;
+}
+
 function addToList() {
-	$('.add-to-list').click(function(e) {
+	$('.pet').on("click", ".add-to-list", function(e) {
 		e.preventDefault();
+		const pet = getPetData($(this).closest(".pet"));
+		$.ajax({
+			type: "POST",
+			url: "https://adopt-list.herokuapp.com/list",
+			data: JSON.stringify(pet),
+			dataType: "json",
+			contentType: "application/json"
+		})
+		.done(function(e) {
+			console.log(e);
+		});
 	});
 }
 
