01184b884a2c47be0056c928cee83d7b5df5726e
mikebcbc <mikebconstantino@gmail.com>
2017-08-11 17:39:40 -0400

    Clientside login HTML

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ const router = express.Router();
 const rp = require('request-promise-native');
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.render('index', { text: 'Hello World' });
 });
 
-router.get('/browse', function(req, res) {
+router.get('/browse', (req, res) => {
 	rp({uri: 'http://localhost:3000/pets', json: true}) // Change request to client side (jquery)
 	  .then(function(pets) {
 	    res.render('browse', {data: pets, title: 'Browse' });
 	  });
+})
+
+router.get('/login', (req, res) => {
+	res.render('login');
 })
 
 module.exports = router;
