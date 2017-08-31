930ab703eba7864376862350159a8dc5813ed079
mikebcbc <mikebconstantino@gmail.com>
2017-08-30 20:27:49 -0400

    Check if pet is in list already

--- public/javascripts/browse-client.js
+++ public/javascripts/browse-client.js
@@ -5,8 +5,8 @@ var RESULT_TEMPLATE = (
 		'<div class="name"></div>' +
 		'<div class="description"><p></p></div>' +
 		'<div class="action">' +
-			'<a href class="add-to-list">ADD TO LIST</a>' +
-			'<a href class="contact-shelter">CONTACT SHELTER</a>' +
+			'<a href class="action-button"></a>' +
+			'<a href class="contact-shelter">CONTACT SHELTER<div class="tool-tip">Email: <p class="email"></p> Phone: <p class="phone"></p></div></a>' +
 		'</div>' +
 	'</div>'
 );
@@ -17,14 +17,25 @@ var PETS_ARRAY;
 
 var PETS_IN_LIST;
 
-// function checkPet(pet) {
-// };
+function checkPet(petId) {
+	return -1 < PETS_IN_LIST.findIndex(function(pet) {
+		return pet.petId == petId;
+	})
+};
 
-function renderPet(pet) {
+function renderPet(pet) { // Change styling between two buttons.
+	var petExists = checkPet(pet.id.$t);
 	var template = $(RESULT_TEMPLATE);
+	if (!petExists) {
+		template.find('.action-button').addClass("add-to-list").text('ADD TO LIST');
+	} else {
+		template.find('.action-button').addClass("remove-from-list").text('REMOVE FROM LIST');
+	}
 	template.attr("data-id", pet.id.$t);
 	template.attr("data-phone", pet.contact.phone.$t);
 	template.attr("data-email", pet.contact.email.$t);
+	template.find(".tool-tip .phone").text(pet.contact.phone.$t);
+	template.find(".tool-tip .email").text(pet.contact.email.$t);
 	template.find(".contact-shelter").attr('href', "mailto:" + pet.contact.email.$t);
 	template.find(".photo").css('background-image', 'url("' + pet.media.photos.photo[0] + '")');
 	template.find(".name").text(pet.name.$t);
@@ -102,26 +113,29 @@ function addToList() {
 			contentType: "application/json"
 		})
 		.done(function(e) {
+			console.log(e);
 			$('.pet[data-id="' + e.petId + '"] .add-to-list').attr("class", "remove-from-list").text("REMOVE FROM LIST");
+			$('.pet[data-id="' + e.petId + '"]').attr('data-mid', e._id);
 		});
 	});
 };
 
 function removeFromList() {
 	$('.adoptable-pets').on("click", ".remove-from-list", function(e) {
+		console.log(e);
 		e.preventDefault();
 		e.stopImmediatePropagation(); // Why does this not work?
 		var pet = $(this).closest(".pet");
 		$.ajax({
 			type: "DELETE",
-			url: "http://localhost:3000/list/" + pet.attr('data-id'),
+			url: "http://localhost:3000/list/" + pet.attr('data-mid'),
 			headers: {
 				'Authorization': 'Bearer ' + localStorage.getItem('authToken')
 			},
 			contentType: "application/json",
 		})
 		.done(function(e) {
-			pet.remove();
+			pet.find('.remove-from-list').attr("class", "add-to-list").text("ADD TO LIST");
 		})
 		.fail(function(err) {
 			console.log(err);
@@ -153,5 +167,6 @@ $(function() {
 	fetchPets();
 	expandPet();
 	addToList();
+	removeFromList();
 	contactShelter();
 })
